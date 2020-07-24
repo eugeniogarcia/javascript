71fe6c1519c5eab426a9496f47ca7e0373005d3d
Eugenio Garcia San Martin <egsmartin@gmail.com>
2020-07-24 07:51:33 +0200

    primera version

---
+++ app/readme.md
@@ -0,0 +1,21 @@
+# Install
+
+```ps
+npm install -s react-native-gesture-handler react-native-markdown-renderer react-native-reanimated react-navigation react-navigation-stack react-navigation-tabs react-native-screens 
+```
+
+```ps
+npm install -s styled-components
+```
+
+```ps
+npm install -s graphql @apollo/client apollo-link-context react-apollo
+```
+
+```ps
+npm install -s expo-secure-store
+```
+
+```ps
+npm install -s date-fns
+```
---
+++ app/src/components/Note.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, ScrollView } from 'react-native';
+import styled from 'styled-components/native';
+import Markdown from 'react-native-markdown-renderer';
+import { format, parseISO } from 'date-fns';
+
+const NoteView = styled.ScrollView`
+  padding: 10px;
+`;
+
+//        {format(new Date(note.createdAt), 'MMM do yyyy')}
+
+const NoteComponent = ({ note }) => {
+  return (
+    <NoteView>
+      <Text>
+        Note by {note.author.username} / Published{' '}
+        {format(parseISO(note.createdAt), 'MMM Do yyyy')}
+      </Text>
+      <Markdown>{note.content}</Markdown>
+    </NoteView>
+  );
+};
+
+export default NoteComponent;
